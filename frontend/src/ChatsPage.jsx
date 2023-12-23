61f25d84ce208db446e1edab1db32334bcf52733
ColinHCook <chc5197@psu.edu>
2023-12-23 12:30:57 -0500

    Chat App v1

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,22 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "23546fd8-5e2b-45f4-9424-28b80b571c68",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
